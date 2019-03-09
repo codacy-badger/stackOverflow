9353f3094526eff13578fa5a4705f30ff77fe617
Darius Njihia Kihuha <darius@Dariuss-MacBook-Pro.local>
2019-03-09 19:25:59 +0300

    Relate answers model to questions model

--- API/answers/models.py
+++ API/answers/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from questions.models import Question
 
 
 # Create your models here.
 class Answer(models.Model):
     content = models.TextField()
+    question = models.ForeignKey(
+        Question, 
+        on_delete=models.CASCADE, default=None
+    )
