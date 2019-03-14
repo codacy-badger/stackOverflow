fab5ac0329c68dea70ae500d4999973ea96d7dc2
Darius Njihia <darius@Dariuss-MacBook-Pro.local>
2019-03-14 19:57:03 +0300

    [Feature #164637874] Configure Read, Update and Delete for specific questions

--- API/questions/tests.py
+++ API/questions/tests.py
@@ -13,15 +13,85 @@ class TestQuestions(TestCase):
                 "title": "Test Question",
                 "content": "This is a question to test the endpoint"
             }
-
-    def test_create_questions(self):
-        response = self.client.post(
+        self.create = self.client.post(
             reverse('create_question'),
             data=self.data,
             format="json"
         )
 
-        record = Question.objects.get(title="Test Question")
+        self.record = Question.objects.get(title="Test Question")
 
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(record.title, self.data['title'])
+    def test_create_questions(self):
+        self.assertEqual(self.create.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(self.record.title, self.data['title'])
+
+    def test_read_specific_question(self):
+        get_response = self.client.get(
+            reverse(
+                'question_details',
+                kwargs={'pk': self.record.id}
+            ),
+            format="json"
+        )
+
+        self.assertEqual(get_response.status_code, status.HTTP_200_OK)
+
+    def test_update_specific_question(self):
+        put_response = self.client.put(
+            reverse(
+                'question_details',
+                kwargs={'pk': self.record.id}
+            ),
+            data={
+                "title": "New title",
+                "content": "New content"
+            },
+            format="json"
+        )
+
+        self.assertEqual(put_response.status_code, status.HTTP_200_OK)
+        self.assertNotEqual(put_response.data['title'], self.data['title'])
+        self.assertEqual(put_response.data['title'], 'New title')
+
+    def test_delete_specific_question(self):
+        new_record = Question.objects.create(
+            title="Should Delete",
+            content="Please pass and delete me"
+        )
+
+        res_before = self.client.get(
+            reverse(
+                'question_details',
+                kwargs={
+                    "pk": new_record.id
+                }
+            ),
+            format="json"
+        )
+
+        del_response = self.client.delete(
+            reverse(
+                'question_details',
+                kwargs={'pk': new_record.id}
+            ),
+            format="json"
+        )
+
+        res_after = self.client.get(
+            reverse(
+                'question_details',
+                kwargs={
+                    "pk": new_record.id
+                }
+            ),
+            format="json"
+        )
+
+        get_record_deleted = Question.objects.filter(title="Should Delete")
+
+        self.assertFalse(get_record_deleted)
+        self.assertEqual(res_before.status_code, status.HTTP_200_OK)
+        self.assertEqual(res_after.status_code, status.HTTP_404_NOT_FOUND)
+
+    def tearDown(self):
+        self.record.delete()
--- API/questions/urls.py
+++ API/questions/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import QuestionsView
+from .views import QuestionsView, QuestionsDetailsView
 
 urlpatterns = [
-    path('', QuestionsView.as_view(), name="create_question")
+    path('', QuestionsView.as_view(), name="create_question"),
+    path('<int:pk>', QuestionsDetailsView.as_view(), name="question_details")
 ]
